3920dd9f48aaeff3d2827e61e37a23179900c44b
Giorgi Patsia <62309418+gio213@users.noreply.github.com>
2023-06-07 12:48:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -126,9 +126,9 @@ const createCard = (number) => {
   card.appendChild(youtubeSpotifyDiv);
   const youtubeIcon = document.createElement("img");
   youtubeIcon.setAttribute("class", "youtubeIcon");
-  youtubeIcon.src = "https://github.com/gio213/The-Collection/blob/main/imgs/youtube-icon.png";
+  youtubeIcon.src = "imgs/youtube-icon.png";
   youtubeSpotifyDiv.appendChild(youtubeIcon);
-  const spotifyIcon = document.createElement("img");
+  const spotifyIcon = document.createElement("img"); 
   spotifyIcon.setAttribute("class", "spotifyIcon");
 
   spotifyIcon.src = "/imgs/spotify-icon.png";
